d9082a5e629dc495b5be63a0501e6ad72c639b35
Saudalnamlah <Saudalnamlah@hotmail.com>
2026-02-11 21:22:33 +0300

    add simple 3-file example

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-print("hello");
+#include <iostream>
+
+// Forward declarations
+class Class1;
+class Class2;
+
+// Include implementation files directly (temporary, simple)
+#include "class1.cpp"
+#include "class2.cpp"
+
+int main() {
+    Class1 c1;
+    Class2 c2;
+
+    c1.sayHello();
+    c2.sayHello();
+
+    return 0;
+}
